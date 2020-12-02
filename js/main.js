5bb4f0776e13da7dcbada762477cf380fac4c3b1
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-12-02 12:53:20 +0100

    adding social list v-for

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const app = new Vue ({
+    el:'#app',
+
+    data: {
+
+        // link list header
+        links: [
+            'Courses',
+            'Course Formats',
+            'Add Course',
+            'Pages',
+            'Demos'
+        ],
+
+        // social list icons
+        socials: [
+            { facebook: 'fab fa-facebook-square' },
+            { facebook: 'fab fa-facebook-square' },
+            { facebook: 'fab fa-facebook-square' },
+        ],
+    },
+})
